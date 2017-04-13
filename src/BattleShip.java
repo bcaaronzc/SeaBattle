86ade6e802ab2eb1642bc6f3893cb5fdef0a914e
Aaron <panyitao1997@hotmail.com>
2017-04-13 22:39:03 +0800

    Create the initializer for BattleShip. Still have not clue how to do this. End today's job.

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -1,5 +1,11 @@
 public class BattleShip {
+	int size;
+	int loc[][];
 	
+	BattleShip(int initSize, int[][] initLoc){
+		size = initSize;
+		loc = initLoc;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,5 @@
 public class Game {
-	
+	int gameBoard[][] = new int[7][7];
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
